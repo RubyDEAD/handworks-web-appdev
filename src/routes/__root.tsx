8e203db7db0b8cee4a208fd86efce0fe73d22e53
Gnashal <yousifceballos@gmail.com>
2025-08-29 17:44:46 +0800

    init: shadcn config, custom tailwind tokens, reat-icons

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,14 +4,14 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
+      <div className="p-2 flex gap-2 bg-amber-50">
+        <Link to="/" className="[&.active]:font-bold text-accent">
           Home
         </Link>
-        <Link to="/about" className="[&.active]:font-bold">
+        <Link to="/about" className="[&.active]:font-bold  text-accent">
           About
         </Link>
-        <Link to='/test' className='[&.active]:font-bold'>
+        <Link to='/test' className='[&.active]:font-bold  text-accent'>
           Test
         </Link>
       </div>
